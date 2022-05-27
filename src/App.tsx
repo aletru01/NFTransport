ddf8db5d90693d702dc5e7e951576499ccffaa3a
Spoyte <104215259+Spoyte@users.noreply.github.com>
2022-05-28 00:45:29 +0200

    extra

--- src/App.tsx
+++ src/App.tsx
@@ -96,7 +96,7 @@ function App() {
       </header>
       <NftGallery
         ownerAddress="0xcbeb03588231c3910d86b2f418453614ed799914"
-        apiUrl="https://testnets-api.opensea.io" />;
+        apiUrl="https://testnets-api.opensea.io" />
     </div>
   );
 }
